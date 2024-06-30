b4196b42a93ca2adb9cf5caa7cd866796d828292
Eleazar Franco <eleazarmfranco@gmail.com>
2024-06-30 16:47:42 +0800

    .

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Page extends CI_Controller {
 
-	public function index()
-	{
+	public function index() {
 		$this->load->view('index');
 	}
 
---
+++ application/models/Units.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Units extends CI_Model {
+
+	public function index() {
+		
+	}
+}
