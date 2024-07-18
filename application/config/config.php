df14dfa6cee38ef4e47998efa9bc6065414a3f28
Eleazar Franco <eleazarmfranco@gmail.com>
2024-07-18 17:40:29 +0800

    .

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/condo/';
-$config['base_url'] = 'https://sandybrown-llama-658903.hostingersite.com/';
+$config['base_url'] = 'https://sandybrown-llama-658903.hostingersite.com/condo';
 
 /*
 |--------------------------------------------------------------------------
