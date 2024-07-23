1569dce4c5ea4b5cb493653ec33be741fe2756cf
Dexter Aragon <xedprogrammer@gmail.com>
2024-07-23 09:10:16 +0800

    fix mobile view on user schedule

--- application/controllers/Units_.php
+++ application/controllers/Units_.php
@@ -1,42 +1,47 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Units_ extends CI_Controller {
+class Units_ extends CI_Controller
+{
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct();
 		$this->load->model('Units');
 	}
 
-	public function getUnitsGallery() {
+	public function getUnitsGallery()
+	{
 		$id = $this->input->post('id');
 		$data = $this->Units->getUnitsGallery($id);
 		$return = '<h5>Unit Gallery</h5>';
 		$return .= '<div class="row">';
 		foreach ($data as $value) {
-		$return .= '<div class="col-3 mb-3">
+			$return .= '<div class="col-lg-4 col-md-4 col-sm-6 mb-3">
 						<a href="#" target="_blank" class="my_unit_image"> 
-							<img class="img-fluid" src="data:'.$value->mime.';base64,'.$value->data.'" alt="" width="100%" style="border-radius: 10px">
+							<img class="img-fluid" src="data:' . $value->mime . ';base64,' . $value->data . '" alt="" width="100%" style="border-radius: 10px">
 						</a>
             		</div>';
 		}
 		$return .= '</div>';
 
-		$return .= '<hr><h5>Ameneties</h5>';
+		$return .= '<hr><h5>Ameneties</h5>
+					<div class="row">';
 		$data = $this->Units->getUnitsAmeneties($id);
 		foreach ($data as $value) {
-			$return .= '<div class="col-12 mb-3">
-							<span class="badge badge-secondary">'.$value->name.' </span>
+			$return .= '<div class="col-3 mb-3">
+							<span class="badge badge-secondary">' . $value->name . ' </span>
 						</div>';
-			}
-			$return .= '</div>';
+		}
+		$return .= '</div></div>';
 		echo $return;
 	}
 
-	public function get_units_gallery() {
+	public function get_units_gallery()
+	{
 		$id = $this->input->post('id');
 		$data = $this->Units->getUnitsGallery($id);
-		
+
 		echo json_encode($data);
 	}
 }
--- application/views/user/schedule.php
+++ application/views/user/schedule.php
@@ -3,23 +3,22 @@
         <div class="row">
             <h1>Schedule</h1>
             <div class="col-12 mb-4">
-                <div class="row col-6">
-                    <div class="col-6">
+                <div class="row col-lg-6 col-md-6 col-sm-12">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
                         <label for="from">From</label>
                         <input type="text" class="form-control" id="from" readonly>
                     </div>
-                    <div class="col-6">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
                         <label for="to">To</label>
                         <input type="text" class="form-control" id="to" readonly>
                     </div>
                 </div>
             </div>
             <div class="col-12 mb-4">
+                <span class="text-danger mb-4">Reminder: If you are browsing in mobile, please press and hold the date. If more than 1 day, press, hold, and drag to desired date</span>
                 <div id="calendar"></div>
             </div>
-            <div class="row" id="units-container">
-                
-            </div>
+            <div class="row" id="units-container"></div>
         </div>
     </div>
     <form id="schedule_detail_form" action="<?= site_url('user/preview_schedue_details')?>" method="POST">
