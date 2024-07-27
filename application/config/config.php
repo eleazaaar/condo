c666b89658d2faea8d022b04b58db2b15d5d3b04
Eleazar Franco <eleazarmfranco@gmail.com>
2024-07-27 16:43:44 +0800

    update hosting

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/condo/';
-// $config['base_url'] = 'https://sandybrown-llama-658903.hostingersite.com/condo';
+// $config['base_url'] = 'http://localhost/condo/';
+$config['base_url'] = 'https://azurecozystays.online/condo/';
 
 /*
 |--------------------------------------------------------------------------
