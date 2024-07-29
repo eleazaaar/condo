3b9aaccaeadd091c7610a99973282a0bda49e723
Dexter Aragon <xedprogrammer@gmail.com>
2024-07-30 07:26:29 +0800

    uncommented script for bootsrapbundle

--- application/views/layout/user.php
+++ application/views/layout/user.php
@@ -174,7 +174,7 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <!-- <script src="<?= base_url() ?>/assets-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+    <script src="<?= base_url() ?>/assets-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
     <script src="<?= base_url('assets/plugins/jquery-ui/jquery-ui.min.js'); ?>"></script>
