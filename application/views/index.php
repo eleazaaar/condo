ed9b236ea6a9f45d5e4c86cfc43bcdb32d3eb39b
Eleazar Franco <eleazarmfranco@gmail.com>
2024-08-12 19:30:05 +0800

    .

--- application/views/index.php
+++ application/views/index.php
@@ -710,7 +710,7 @@
   </footer>
 
   <!-- Scroll Top -->
-  <a id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Preloader -->
   <div id="preloader"></div>
